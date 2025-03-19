39aeedad715a82adcb1c0d585d3fa7d605e68c63
void-alive <sja9284@gmail.com>
2025-03-19 16:44:32 +0900

    이게뭐냐

--- app/app/src/main/java/bitc/fullstack/FlightLog/MainActivity.kt
+++ app/app/src/main/java/bitc/fullstack/FlightLog/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
 //    심지현
-//    심지현심지현심지현심지현
+//    심지현2
     setContentView(R.layout.activity_main)
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
