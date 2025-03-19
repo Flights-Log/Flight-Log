8073853a80b9bbd3bab1069234c3940525708fa4
gaggmin <gkdlscm1@naver.com>
2025-03-19 15:11:35 +0900

    프로젝트 앱/서버 파일만 생성

---
+++ server/FlightLog-server/src/main/java/bitc/fullstack503/flightlog/flightlogserver/FlightLogServerApplication.java
@@ -0,0 +1,13 @@
+package bitc.fullstack503.flightlog.flightlogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightLogServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlightLogServerApplication.class, args);
+  }
+
+}
