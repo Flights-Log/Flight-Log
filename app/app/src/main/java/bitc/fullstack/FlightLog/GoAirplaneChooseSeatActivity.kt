bcb9ed7b5c6788baf950bee16b7a9396f614f543
void-alive <sja9284@gmail.com>
2025-03-20 16:48:04 +0900

    좌석

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/GoAirplaneChooseSeatActivity.kt
@@ -0,0 +1,29 @@
+package bitc.fullstack.FlightLog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.databinding.ActivityComeAirplaneBinding
+import bitc.fullstack.FlightLog.databinding.ActivityGoAirplaneChooseSeatBinding
+
+class GoAirplaneChooseSeatActivity : AppCompatActivity() {
+  private val binding: ActivityGoAirplaneChooseSeatBinding by lazy {
+    ActivityGoAirplaneChooseSeatBinding.inflate(layoutInflater)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+
+
+  }
+}
