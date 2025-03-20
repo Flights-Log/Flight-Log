be82a847b09a7bb516b7f5d77604412d1fa48d1f
void-alive <sja9284@gmail.com>
2025-03-20 14:08:44 +0900

    MainActivity 화면 제작 완료

--- app/app/src/main/java/bitc/fullstack/FlightLog/MainActivity.kt
+++ app/app/src/main/java/bitc/fullstack/FlightLog/MainActivity.kt
@@ -1,24 +1,50 @@
 package bitc.fullstack.FlightLog
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  //  ActivityMainBinding
+  private val binding: ActivityMainBinding by lazy {
+    ActivityMainBinding.inflate(layoutInflater)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-//    심지현
-//    심지현22323
-    setContentView(R.layout.activity_main)
+    setContentView(binding.root)
 
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//    툴바 아이콘
+    val menuButton: ImageButton = findViewById(R.id.flight_log_menu)
+    val iconButton: ImageView = findViewById(R.id.flight_log_icon)
+    val loginButton: TextView = findViewById(R.id.flight_log_login)
+
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
 
+//    ClickListener
+    menuButton.setOnClickListener {
+      Log.d("flightLog", "메뉴 버튼 누름")
+    }
+
+    iconButton.setOnClickListener {
+      Log.d("flightLog", "아이콘 버튼 누름")
+    }
+
+    loginButton.setOnClickListener {
+      Log.d("flightLog", "로그인 버튼 누름")
+    }
   }
 }
