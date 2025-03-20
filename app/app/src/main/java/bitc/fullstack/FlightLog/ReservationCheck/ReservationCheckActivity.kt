1f6795ca531d2e38a909f9aa9f90eb0d2e0ddc6d
donghoon1634 <tkdduq010@naver.com>
2025-03-20 17:29:26 +0900

    bitc.fullstack.FlightLog 밑에 ReservationCheck, NonMember 패키지 만듦
    <ReservationCheck XML> - 예약 확정 화면
    
    <View 태그>
    굵기 = 1dp
    라인 선 색 = #868686 (회색)
    마진TOP = 5dp
    
    [항공권 번호]
    id et_tictek_number
    
    [출발지]
    id et_from
    
    [도착지]
    id et_to
    
    [출발 시간]
    id et_departure_time
    
    [탑승인원]
    id et_passengers
    
    [좌석 등급]
    id et_seat_grade
    
    [좌석 번호]
    id et_seat_number
    
    [예약번호]
    id et_reservation_number
    
    [메인으로 가기]
    id btn_home
    
    <ReservationCheck Activity >
    바인딩
    
      initEventListener()
    
      private fun initEventListener() {  }
    만 추가
    
    ======================================================================
    
    [build.gradle.kts 종속성 추가]
    // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
    implementation("com.squareup.retrofit2:retrofit:2.11.0")
    
    // https://mvnrepository.com/artifact/com.squareup.retrofit2/converter-gson
    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
    
    // https://mvnrepository.com/artifact/com.google.code.gson/gson
    implementation("com.google.code.gson:gson:2.12.1")
    
    // https://mvnrepository.com/artifact/com.squareup.retrofit2/converter-scalars
    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
    
    [Manifest 파일]
    
    <퍼미션 추가>
    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
    <uses-permission android:name="android.permission.INTERNET" />
    
    <application 태그 부분>
    테마 밑에
    android:usesCleartextTraffic="true" 이거 1줄 추가
    
    ======================================================================
    
    <Nonmember> - 비회원 조회 화면
    
    [비회원 예약 번호]
    id nonmember_reservation_number
    
    [비회원 예약 조회 비밀번호]
    id nonmember_pw
    
    ======================================================================
    
    <Passemger> - 탑승객 정보 입력 화면
    <edittext_border.xml 추가>
    (edittext 네모 박스 안에 적게 하려고 추가)
    
    [신분증상 성별]
    여성 - id female
    남성 - id male

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/ReservationCheck/ReservationCheckActivity.kt
@@ -0,0 +1,34 @@
+package bitc.fullstack.FlightLog.ReservationCheck
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.R
+import bitc.fullstack.FlightLog.databinding.ActivityReservationCheckBinding
+
+class ReservationCheckActivity : AppCompatActivity() {
+
+  private val binding: ActivityReservationCheckBinding by lazy{
+    ActivityReservationCheckBinding.inflate(layoutInflater)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+
+    initEventListener()
+  }
+
+  private fun initEventListener() {
+
+
+  }
+}
