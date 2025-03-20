b6d5b8d161aaa8c18667ba0958fd69a864a083a6
void-alive <sja9284@gmail.com>
2025-03-20 15:55:23 +0900

    가는 항공편, 오는 항공편 조회

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/ComeActivityAirplane.kt
@@ -0,0 +1,36 @@
+package bitc.fullstack.FlightLog
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.databinding.ActivityComeAirplaneBinding
+import bitc.fullstack.FlightLog.databinding.ActivityGoAirplaneBinding
+
+class ComeActivityAirplane : AppCompatActivity() {
+  //  bitc.fullstack.FlightLog.databinding.ActivityComeAirplaneBinding
+  private val binding: ActivityComeAirplaneBinding by lazy {
+    ActivityComeAirplaneBinding.inflate(layoutInflater)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+
+    //    툴바 아이콘
+    val menuButton: ImageButton = findViewById(R.id.flight_log_menu)
+    val iconButton: ImageView = findViewById(R.id.flight_log_icon)
+    val loginButton: TextView = findViewById(R.id.flight_log_login)
+
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
