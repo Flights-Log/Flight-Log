4043ee518c3fe818e3bad2bfb53c9d8cd2fdaa0d
void-alive <sja9284@gmail.com>
2025-03-21 15:13:30 +0900

    버튼 크기 전부 다 50dp 로 수정

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/ReservationConfirmActivity.kt
@@ -0,0 +1,28 @@
+package bitc.fullstack.FlightLog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.databinding.ActivityReservationConfirmBinding
+import bitc.fullstack.FlightLog.databinding.ActivityTicketHolderBinding
+
+class ReservationConfirmActivity : AppCompatActivity() {
+  //  ActivityMainBinding
+  private val binding: ActivityReservationConfirmBinding by lazy {
+    ActivityReservationConfirmBinding.inflate(layoutInflater)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
