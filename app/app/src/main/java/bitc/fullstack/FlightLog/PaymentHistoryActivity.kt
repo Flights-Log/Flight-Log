caa00b1127eec8cffdd7ff8f85dbd429d669cad4
void-alive <sja9284@gmail.com>
2025-03-21 14:17:51 +0900

    예약확정 뷰 페이지

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/PaymentHistoryActivity.kt
@@ -0,0 +1,27 @@
+package bitc.fullstack.FlightLog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.databinding.ActivityPaymentHistoryBinding
+import bitc.fullstack.FlightLog.databinding.ActivityTicketHolderBinding
+
+class PaymentHistoryActivity : AppCompatActivity() {
+  //  ActivityMainBinding
+  private val binding: ActivityPaymentHistoryBinding by lazy {
+    ActivityPaymentHistoryBinding.inflate(layoutInflater)
+  }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(binding.root)
+
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
