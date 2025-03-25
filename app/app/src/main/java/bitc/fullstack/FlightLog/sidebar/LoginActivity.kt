57c8739e6c669baf5f129092321ccfbede6b9a59
keung_min <gkdlscm1@naver.com>
2025-03-25 12:43:56 +0900

    로그인 - 사이드바 화면

---
+++ app/app/src/main/java/bitc/fullstack/FlightLog/sidebar/LoginActivity.kt
@@ -0,0 +1,36 @@
+package bitc.fullstack.FlightLog.sidebar
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.FlightLog.R
+import bitc.fullstack.FlightLog.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    //    툴바 아이콘
+    val menuButton: ImageButton = findViewById(R.id.flight_log_menu)
+    val iconButton: ImageView = findViewById(R.id.flight_log_icon)
+    val loginButton: TextView = findViewById(R.id.flight_log_login)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
